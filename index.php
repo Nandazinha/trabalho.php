ff985dcdd7ea564d42284378bf2e5cd84e9ac8f5
Nandazinha <fernandalves014@gmail.com>
2025-05-21 13:26:02 -0300

    final

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Vôlei</title>
     <link rel="icon" href="img/loguinho.avif" type="image/png">
-    
+
 </head>
+
 <body>
 
     <header>
@@ -18,27 +20,27 @@
         <nav>
             <ul>
                 <li>
-                    <a href="index.html">Início</a>
+                    <a href="index.php">Início</a>
                 </li>
                 <li>
-                    <a href="comentarios.html">Comentários</a>
+                    <a href="comentarios.php">Comentários</a>
                 </li>
                 <li>
-                    <a href="contato.html">Contato</a>
+                    <a href="contato.php">Contato</a>
                 </li>
             </ul>
         </nav>
-    
+
     </header>
     <hr>
-    
-
-<center>
-    
-    <h1>Vôlei e suas características</h1>
 
 
-</center>
+    <center>
+
+        <h1>Vôlei e suas características</h1>
+
+
+    </center>
 
     <section>
         <div class="conteudo">
@@ -54,71 +56,72 @@
 
                 <h2>Características</h2>
 
-                 <p>O vôlei, ou voleibol, é um esporte coletivo que envolve duas equipes separadas por uma rede, cujo objetivo principal é fazer a bola tocar o chão da quadra adversária. Criado em 1895 por William G. Morgan, o esporte combina habilidades como força, agilidade, coordenação e trabalho em equipe, sendo praticado em diversos países ao redor do mundo. Ele pode ser jogado tanto em ambientes fechados quanto ao ar livre, como na modalidade de vôlei de praia.</p>
+                <p>O vôlei, ou voleibol, é um esporte coletivo que envolve duas equipes separadas por uma rede, cujo objetivo principal é fazer a bola tocar o chão da quadra adversária. Criado em 1895 por William G. Morgan, o esporte combina habilidades como força, agilidade, coordenação e trabalho em equipe, sendo praticado em diversos países ao redor do mundo. Ele pode ser jogado tanto em ambientes fechados quanto ao ar livre, como na modalidade de vôlei de praia.</p>
 
-    <p>Uma das principais características do vôlei é a sua dinâmica de jogo, que exige reflexos rápidos e estratégias bem definidas. Cada equipe é composta por seis jogadores em quadra, que se revezam em posições específicas, como levantador, atacante e líbero. O jogo é dividido em sets, e vence a equipe que alcançar primeiro 25 pontos, com uma diferença mínima de dois pontos em relação ao adversário.</p>
+                <p>Uma das principais características do vôlei é a sua dinâmica de jogo, que exige reflexos rápidos e estratégias bem definidas. Cada equipe é composta por seis jogadores em quadra, que se revezam em posições específicas, como levantador, atacante e líbero. O jogo é dividido em sets, e vence a equipe que alcançar primeiro 25 pontos, com uma diferença mínima de dois pontos em relação ao adversário.</p>
 
-    <p>Além disso, o vôlei é conhecido por suas regras específicas, como o limite de três toques por equipe antes de enviar a bola para o lado adversário. Esses toques geralmente incluem o passe, o levantamento e o ataque, que são executados de forma coordenada para maximizar as chances de pontuar. A comunicação entre os jogadores é essencial para o sucesso da equipe, tornando o esporte um excelente exemplo de cooperação e trabalho em grupo.</p>
+                <p>Além disso, o vôlei é conhecido por suas regras específicas, como o limite de três toques por equipe antes de enviar a bola para o lado adversário. Esses toques geralmente incluem o passe, o levantamento e o ataque, que são executados de forma coordenada para maximizar as chances de pontuar. A comunicação entre os jogadores é essencial para o sucesso da equipe, tornando o esporte um excelente exemplo de cooperação e trabalho em grupo.</p>
 
-    <p>Por fim, o vôlei é um esporte inclusivo e acessível, que pode ser praticado por pessoas de todas as idades e níveis de habilidade. Ele promove benefícios físicos, como o desenvolvimento muscular e cardiovascular, além de contribuir para o bem-estar mental, ao incentivar a socialização e o espírito esportivo. Não é à toa que o vôlei é um dos esportes mais populares e admirados em competições internacionais, como os Jogos Olímpicos.</p>
+                <p>Por fim, o vôlei é um esporte inclusivo e acessível, que pode ser praticado por pessoas de todas as idades e níveis de habilidade. Ele promove benefícios físicos, como o desenvolvimento muscular e cardiovascular, além de contribuir para o bem-estar mental, ao incentivar a socialização e o espírito esportivo. Não é à toa que o vôlei é um dos esportes mais populares e admirados em competições internacionais, como os Jogos Olímpicos.</p>
 
 
             </div>
 
         </div>
         <br>
-<center>
+        <center>
             <h2>Posições e principais rodízios</h2>
-</center>
-<br>
+        </center>
+        <br>
         <div class="cards-container">
             <div class="card">
                 <h3>Levantador</h3>
                 <p>Responsável por controlar o jogo e realizar os levantamentos.</p>
-                <a href="levantador.html" class="saiba-mais">Saiba mais</a>
+                <a href="levantador.php" class="saiba-mais">Saiba mais</a>
             </div>
             <div class="card">
                 <h3>Líbero</h3>
                 <p>É um especialista de defesa,sua função é de defender ataques.</p>
-                <a href="libero.html" class="saiba-mais">Saiba mais</a>
+                <a href="libero.php" class="saiba-mais">Saiba mais</a>
             </div>
             <div class="card">
                 <h3>Ponteiro</h3>
                 <p>Com boa capacidade de defesa e ataque ele pode fazer ambos.</p>
-                <a href="ponteiro.html" class="saiba-mais">Saiba mais</a>
+                <a href="ponteiro.php" class="saiba-mais">Saiba mais</a>
             </div>
             <div class="card">
                 <h3>Oposto</h3>
                 <p>É o principal jogador de ataque, geralmente o com mais força.</p>
-                <a href="oposto.html" class="saiba-mais">Saiba mais</a>
+                <a href="oposto.php" class="saiba-mais">Saiba mais</a>
             </div>
             <div class="card">
                 <h3>Central</h3>
                 <p>Jogador que fica no meio da rede, com a função de bloquear e atacar.</p>
-                <a href="central.html" class="saiba-mais">Saiba mais</a>
+                <a href="central.php" class="saiba-mais">Saiba mais</a>
             </div>
             <div class="card">
                 <h3>Rodízio 6x0</h3>
                 <p>É um sistema tático utilizado na iniciação ao esporte, especialmente para equipes de jovens e iniciantes.</p>
-                <a href="rodizio1.html" class="saiba-mais">Saiba mais</a>
+                <a href="rodizio1.php" class="saiba-mais">Saiba mais</a>
             </div>
             <div class="card">
                 <h3>Rodízio 5x1</h3>
                 <p>É uma estratégia tática em que o time utiliza um levantador (jogador que faz a distribuição da bola) e quatro jogadores de ataque.</p>
-                <a href="rodizio2.html" class="saiba-mais">Saiba mais</a>
+                <a href="rodizio2.php" class="saiba-mais">Saiba mais</a>
             </div>
         </div>
 
     </section>
 
     <center>
-    <footer> 
-        <div class="footer">
-            <p>&copy;Todos os direitos reservados.</p>
-            <p>Desenvolvido por Fernanda</p>
-        </div>
-    </footer>
+        <footer>
+            <div class="footer">
+                <p>&copy;Todos os direitos reservados.</p>
+                <p>Desenvolvido por Fernanda</p>
+            </div>
+        </footer>
     </center>
 
 </body>
+
 </html>
--- libero.php
+++ libero.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Vôlei</title>
     <link rel="icon" href="img/loguinho.avif" type="image/png">
-    
+
 </head>
+
 <body>
 
     <header>
@@ -18,27 +20,27 @@
         <nav>
             <ul>
                 <li>
-                    <a href="index.html">Início</a>
+                    <a href="index.php">Início</a>
                 </li>
                 <li>
-                    <a href="comentarios.html">Comentários</a>
+                    <a href="comentarios.php">Comentários</a>
                 </li>
                 <li>
-                    <a href="contato.html">Contato</a>
+                    <a href="contato.php">Contato</a>
                 </li>
             </ul>
         </nav>
-    
+
     </header>
     <hr>
-    
-
-<center>
-    
-    <h1>LÍBERO</h1>
 
 
-</center>
+    <center>
+
+        <h1>LÍBERO</h1>
+
+
+    </center>
 
     <section>
         <div class="conteudo">
@@ -55,7 +57,7 @@
                 <h2>Características</h2>
 
                 <p>O líbero é um jogador especializado na defesa e recepção, não podendo atacar ou sacar. Ele usa um uniforme diferente para se destacar e pode substituir qualquer jogador na linha de defesa sem necessidade de pedido formal ao árbitro. Sua função exige reflexos rápidos, excelente controle de bola e habilidade em passes precisos para organizar a equipe defensivamente.</p>
-                
+
                 <h2>Curiosidades</h2>
 
                 <ul>
@@ -72,55 +74,56 @@
 
 
     <center>
-            <h2>OUTROS</h2>
-</center>
-<br>
-        <div class="cards-container">
-            <div class="card">
-                <h3>Levantador</h3>
-                <p>Responsável por controlar o jogo e realizar os levantamentos.</p>
-                <a href="levantador.html" class="saiba-mais">Saiba mais</a>
-            </div>
-            <div class="card">
-                <h3>Líbero</h3>
-                <p>É um especialista de defesa,sua função é de defender ataques.</p>
-                <a href="libero.html" class="saiba-mais">Saiba mais</a>
-            </div>
-            <div class="card">
-                <h3>Ponteiro</h3>
-                <p>Com boa capacidade de defesa e ataque ele pode fazer ambos.</p>
-                <a href="ponteiro.html" class="saiba-mais">Saiba mais</a>
-            </div>
-            <div class="card">
-                <h3>Oposto</h3>
-                <p>É o principal jogador de ataque, geralmente o com mais força.</p>
-                <a href="oposto.html" class="saiba-mais">Saiba mais</a>
-            </div>
-            <div class="card">
-                <h3>Central</h3>
-                <p>Jogador que fica no meio da rede, com a função de bloquear e atacar.</p>
-                <a href="central.html" class="saiba-mais">Saiba mais</a>
-            </div>
-            <div class="card">
-                <h3>Rodízio 6x0</h3>
-                <p>É um sistema tático utilizado na iniciação ao esporte, especialmente para equipes de jovens e iniciantes.</p>
-                <a href="rodizio1.html" class="saiba-mais">Saiba mais</a>
-            </div>
-            <div class="card">
-                <h3>Rodízio 5x1</h3>
-                <p>É uma estratégia tática em que o time utiliza um levantador (jogador que faz a distribuição da bola) e quatro jogadores de ataque.</p>
-                <a href="rodizio2.html" class="saiba-mais">Saiba mais</a>
-            </div>
+        <h2>OUTROS</h2>
+    </center>
+    <br>
+    <div class="cards-container">
+        <div class="card">
+            <h3>Levantador</h3>
+            <p>Responsável por controlar o jogo e realizar os levantamentos.</p>
+            <a href="levantador.php" class="saiba-mais">Saiba mais</a>
         </div>
+        <div class="card">
+            <h3>Líbero</h3>
+            <p>É um especialista de defesa,sua função é de defender ataques.</p>
+            <a href="libero.php" class="saiba-mais">Saiba mais</a>
+        </div>
+        <div class="card">
+            <h3>Ponteiro</h3>
+            <p>Com boa capacidade de defesa e ataque ele pode fazer ambos.</p>
+            <a href="ponteiro.php" class="saiba-mais">Saiba mais</a>
+        </div>
+        <div class="card">
+            <h3>Oposto</h3>
+            <p>É o principal jogador de ataque, geralmente o com mais força.</p>
+            <a href="oposto.php" class="saiba-mais">Saiba mais</a>
+        </div>
+        <div class="card">
+            <h3>Central</h3>
+            <p>Jogador que fica no meio da rede, com a função de bloquear e atacar.</p>
+            <a href="central.php" class="saiba-mais">Saiba mais</a>
+        </div>
+        <div class="card">
+            <h3>Rodízio 6x0</h3>
+            <p>É um sistema tático utilizado na iniciação ao esporte, especialmente para equipes de jovens e iniciantes.</p>
+            <a href="rodizio1.php" class="saiba-mais">Saiba mais</a>
+        </div>
+        <div class="card">
+            <h3>Rodízio 5x1</h3>
+            <p>É uma estratégia tática em que o time utiliza um levantador (jogador que faz a distribuição da bola) e quatro jogadores de ataque.</p>
+            <a href="rodizio2.php" class="saiba-mais">Saiba mais</a>
+        </div>
+    </div>
 
     <center>
-    <footer id="footer"> 
-        <div class="footer">
-            <p>&copy;Todos os direitos reservados.</p>
-            <p>Desenvolvido por Fernanda</p>
-        </div>
-    </footer>
+        <footer id="footer">
+            <div class="footer">
+                <p>&copy;Todos os direitos reservados.</p>
+                <p>Desenvolvido por Fernanda</p>
+            </div>
+        </footer>
     </center>
 
 </body>
+
 </html>
